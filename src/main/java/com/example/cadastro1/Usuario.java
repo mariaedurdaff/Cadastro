12637444cf4fd5d12287c96e93751d4d6ba884f1
24105402870 <maryyduarteh@gmail.com>
2026-08-17 10:40:57 -0300

    Primeira API de Cadastro

---
+++ src/main/java/com/example/cadastro1/Usuario.java
@@ -0,0 +1,22 @@
+package com.example.cadastro1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+
+    public class Usuario {
+
+        private String nome;
+        private String email;
+        private String cpf;
+
+    }
+
--- src/main/java/com/example/cadastro1/UsuarioController.java
+++ src/main/java/com/example/cadastro1/UsuarioController.java
@@ -1,4 +1,24 @@
 package com.example.cadastro1;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+    ArrayList<Usuario> usuarios = new ArrayList<>();
+
+    @GetMapping
+    public ArrayList<Usuario> listarTodosUsuarios() {
+        return usuarios;
+    }
+
+    @PostMapping
+    public Usuario cadastrarUsuario(@RequestBody Usuario usuario) {
+        usuarios.add(usuario);
+        return usuarios.getLast();
+    }
 }
