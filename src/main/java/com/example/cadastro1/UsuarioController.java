75edfd02932d92deba1915f396c1fee2ed8074b3
24105402870 <maryyduarteh@gmail.com>
2026-08-24 08:28:05 -0300

    Primeira API de Cadastro

--- src/main/java/com/example/cadastro1/UsuarioController.java
+++ src/main/java/com/example/cadastro1/UsuarioController.java
@@ -21,4 +21,19 @@ public class UsuarioController {
         usuarios.add(usuario);
         return usuarios.getLast();
     }
+
+    @PutMapping("/{id}")
+    public Usuario atualizarUsuario(@PathVariable int id, @RequestBody Usuario usuario) {
+        Usuario usuarioAtualizado = usuarios.get(id);
+
+        usuarioAtualizado.setNome(usuario.getNome());
+        usuarioAtualizado.setEmail(usuario.getEmail());
+        usuarioAtualizado.setCpf(usuario.getCpf());
+
+        return usuarioAtualizado;
+    }
+    @DeleteMapping("/{id}")
+    public void deletarUsuario(@PathVariable int id) {
+        usuarios.remove(id);
+    }
 }
